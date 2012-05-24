53e51fc55a9f1478699dde8ac6f8419ed06ab2e0
Patrick Lodder <patrick.lodder@intellibiz.org>
2012-05-24 16:51:02 +0200

    initial bugfix

--- index.js
+++ index.js
@@ -3,35 +3,43 @@ var url = require('url')
 	, util = require('util')
 	, events = require('events');
 
-function Transaction (req, res, matches, cb) {
+var REQUEST_METHODS = {GET: true, POST: true, DELETE: true};
+
+function Transaction (req, res, cb) {
 	this.req = req;
 	this.res = res;
-	this.matches = (matches === true) ? [] : matches || [];
-	this.body = {};
-	this.chunks = [];
 	this.callback = cb;
 	
-	var self = this;
+	this.ready = false;
 	
-	if (this.req.method.toLowerCase() === 'post') {
-		
-		this.req.on('data', function (c) { 
-			self.chunks.push(c);
-		});
-		
-		this.req.on('end', function () {
-			self.body = qs.parse(self.chunks.join(''));
-		});
-	}
+	this.ee = new events.EventEmitter;
+	
+	this.matches = null;
+	this.body = {};
+	this.chunks = [];
+	
+	this.setReqData = function(chunks, body) {
+		this.chunks = chunks;
+		this.body = body;
+		this.ready = true;
+		this.ee.emit('ready', this);
+	};
 	
 	this.handle = function (func) {
-		var err = false;
-		try {
-			this.call(func);
-		} catch (e) {
-			err = e;
+		if (!this.ready) {
+		
+			this.ee.once('ready', function(that) { that.handle(func); });
+		
+		} else {
+		
+			var err = false;
+			try {
+				func.call(this);
+			} catch (e) {
+				err = e;
+			}
+			cb(err);
 		}
-		cb(err);
 	};
 };
 
@@ -52,7 +60,7 @@ function Route (method, route, controller) {
 
 function Entrance (options) {
 	this.options = options;
-	this.routes = {};
+	this.routes = {all: {}};
 	this.callback = function(){};
 }
 
@@ -70,18 +78,29 @@ Entrance.prototype.add = function (method, route, controller, strict) {
 	} else this.routes[method][key] = r;
 };
 
-Entrance.prototype.dispatch = function (req, res) {
-	var methods = ['all', req.method.toLower()];
+Entrance.prototype.dispatch = function (req, res, cb) {
+	var chunks = [], transaction = new Transaction(req, res, this.callback);
+	req.on('data', function (c) { 
+		chunks.push(c);
+	});
+	req.on('end', function () {
+		transaction.setReqData(chunks, (chunks.length > 0) ? qs.parse(chunks.join('')) : {});
+	});
+	
+	if (cb) this.callback = cb;
+	
+	var methods = ['all', req.method.toLowerCase()];
 	var path = url.parse(req.url).pathname;
 	var i = 2;
 	
 	while (i--) {
 		if (this.routes.hasOwnProperty(methods[i]) && typeof(this.routes[methods[i]]) === 'object') {
-			for (var k in this.routes[method[i]]) {
-				var r = this.routes[method[i]][k], match;
+			for (var k in this.routes[methods[i]]) {
+				var r = this.routes[methods[i]][k], match;
 				
 				if (match = r.match(path)) {
-					(new Transaction(req, res, match, this.callback)).handle(r.controller);
+					if (match !== true) transaction.matches = match;
+					transaction.handle(r.controller);
 					return;
 				}
 			}
@@ -91,5 +110,14 @@ Entrance.prototype.dispatch = function (req, res) {
 	this.callback('No route could be matched');	
 	
 };
+
+//make request method prototypes
+Entrance.createMethod  = function (method) {
+		Entrance.prototype[method] = function(route, controller, strict) {
+			this.add(method, route, controller, strict);
+		};
+};
+
+for (var m in REQUEST_METHODS) {Entrance.createMethod(m.toLowerCase());}
 
 module.exports = Entrance;
